47b7d5c7eb5cbb8d1ea19ac10b0fb4dad53a120f
Imad Ben <imadbenahmed03@gmail.com>
2025-05-03 15:41:43 +0100

    Added the remaining add+edit

--- app/Http/Controllers/Api/V1/DBFunctions.php
+++ app/Http/Controllers/Api/V1/DBFunctions.php
@@ -75,7 +75,7 @@ class DBFunctions extends Controller
 
     //* Adding a new thing */
 
-    public static function store($parent, $data)
+    public static function store($parent, $data, ?int $parentId = null)
     {
 
         // Retrieve the XML from the database
@@ -99,11 +99,55 @@ class DBFunctions extends Controller
         // Add the new account
         switch ($parent) {
             case 'accounts':
+                $newId = count($parentNode->account) + 1;
                 $newAccountNode = $parentNode->addChild('account');
                 $newAccountNode->addAttribute('id', $newId);
                 $newAccountNode->addChild('userName', $data->userName);
                 $newAccountNode->addChild('password', $data->password);
                 $newAccountNode->addChild('email', $data->email);
+                break;
+            case 'userProfile':
+                $newId = count($parentNode->userProfile) + 1;
+                $parentId = $root->xpath("//account[@id='$parentId']")[0] ?? null;
+                $newProfileNode = $parentNode->addChild('userProfile');
+                $newProfileNode->addAttribute('id', $newId);
+                $newProfileNode->addAttribute('idAccount', $parentId);
+                $newProfileNode->addChild('firstName', $data->firstName);
+                $newProfileNode->addChild('lastName', $data->lastName);
+                $newProfileNode->addChild('profilePicture', $data->profilePicture);
+                $newProfileNode->addChild('bio', $data->bio);
+                $newProfileNode->addChild('joined', date('Y-m-d'));
+                break;
+            case 'topic':
+                $newId = count($parentNode->topic) + 1;
+                $newTopicNode = $parentNode->addChild('topic');
+                $newTopicNode->addAttribute('id', $newId);
+                $newTopicNode->addAttribute('idAccount', $parentId);
+                $newTopicNode->addChild('problem', $data->problem);
+                $newTopicNode->addChild('description', $data->description);
+                $newTopicNode->addChild('createdAt', date('Y-m-d'));
+                $newTopicNode->addChild('updatedAt', date('Y-m-d'));
+                $newTopicNode->addChild('steps');
+                if (isset($data->steps->step)) {
+                    foreach ($data->steps->step as $index => $step) {
+                        $stepId = count($parentNode->step) + (int)$index;
+                        $newStepNode = $newTopicNode->steps->addChild('step');
+                        $newStepNode->addAttribute('id', $stepId);
+                        $newStepNode->addAttribute('idTopic', $newId);
+                        $newStepNode->addChild('title', $step->title);
+                        $newStepNode->addChild('description', $step->description);
+                        $newStepNode->addChild('updatedAt', date('Y-m-d'));
+                    }
+                }
+                break;
+            case 'vote': 
+                //(!Important)
+                //Probably needs to change depending on the actual structure of the vote and what is sent
+                $parentId = $root->xpath("//topic[@id='$parentId']")[0] ?? null;
+                $newVoteNode = $parentNode->addChild('vote');
+                $newVoteNode->addAttribute('idTopic', $parentId);
+                $newVoteNode->addAttribute('idAccount', $data->idAccount);
+                $newVoteNode->addChild('isPositive', $data->isPositive);
                 break;
         }
 
@@ -143,6 +187,45 @@ class DBFunctions extends Controller
                 if (isset($data->email))
                     $parentNode->email = $data->email;
                 break;
+            case 'userProfile':
+                if (isset($data->firstName))
+                    $parentNode->firstName = $data->firstName;
+                if (isset($data->lastName))
+                    $parentNode->lastName = $data->lastName;
+                if (isset($data->profilePicture))
+                    $parentNode->profilePicture = $data->profilePicture;
+                if (isset($data->bio))
+                    $parentNode->bio = $data->bio;
+                break;
+            case 'topic':
+                if (isset($data->problem))
+                    $parentNode->problem = $data->problem;
+                if (isset($data->description))
+                    $parentNode->description = $data->description;
+                if (isset($data->steps->step)) {
+                    unset($parentNode->steps);
+                    $parentNode->addChild('steps');
+                    foreach ($data->steps->step as $index => $step) {
+                        $stepId = count($parentNode->step) + (int)$index;
+                        $newStepNode = $parentNode->steps->addChild('step');
+                        $newStepNode->addAttribute('id', $stepId);
+                        $newStepNode->addAttribute('idTopic', $id);
+                        if (isset($step->title))
+                            $newStepNode->title = $step->title;
+                        if (isset($step->description))
+                            $newStepNode->description = $step->description;
+                        if (isset($step->updatedAt))
+                            $newStepNode->updatedAt = date('Y-m-d');
+                    }
+                }
+                break;
+            case 'vote':
+                //(!Important)
+                //The only thing editable in the vote is the text inside the isPositive tag
+                if (isset($data->isPositive)) {
+                    $parentNode->isPositive = $data->isPositive;
+                }
+                break;
         }
 
         // Save the updated XML back to the database
@@ -154,7 +237,7 @@ class DBFunctions extends Controller
     }
 
     // * Deleting a specific thing */
-    public static function destroy($parent, $id)
+    public static function destroy($parent, $id, ?int $idAccount = null)
     {
         // Retrieve the XML from the database
         $xmlData = DB::table('dbxml')->where('id', 1)->value('info');
@@ -164,8 +247,13 @@ class DBFunctions extends Controller
         }
         $root = new \SimpleXMLElement($xmlData);
 
-        // Find the parent node (accounts)
-        $parentNode = $root->xpath("//{$parent}[@id='$id']")[0] ?? null;
+        // Find the parent node (account for example)
+        if ($idAccount) {
+            $parent = $parent . "[(@idTopic=$id and @idAccount=$idAccount)]";
+        } else {
+            $parent = $parent . "[@id=$id]";
+        }
+        $parentNode = $root->xpath("//{$parent}")[0] ?? null;
         if (!$parentNode) {
             return response('<error>Parent node not found</error>', 404)
                 ->header('Content-Type', 'application/xml');
